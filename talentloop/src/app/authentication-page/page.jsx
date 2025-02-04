333d593dd9a6bb5c2f8ae51443ddda5690af75f2
aryannaik225 <aryann2203@gmail.com>
2025-02-04 23:42:58 +0530

    just saving

--- talentloop/src/app/authentication-page/page.jsx
+++ talentloop/src/app/authentication-page/page.jsx
@@ -1,9 +1,15 @@
+import LoginBox from "@/components/authentication-page/LoginBox";
 import Navbar from "@/components/authentication-page/Navbar";
 
 export default function Home () {
   return (
-    <div>
+    <div className="w-screen h-screen authen-bg">
       <Navbar />
+      <div className="flex w-screen h-full">
+        <div className="w-6/12 h-10/12">
+          <LoginBox />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ talentloop/src/components/authentication-page/LoginBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginBox = () => {
+  return (
+    <div className='w-full h-full flex justify-center items-center'>
+      LoginBox
+    </div>
+  )
+}
+
+export default LoginBox
