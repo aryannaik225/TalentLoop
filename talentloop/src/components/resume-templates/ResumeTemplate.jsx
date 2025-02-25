6f5e7f043e14b64a11cd504c74dcf7c4cb6a1eda
aryannaik225 <aryann2203@gmail.com>
2025-02-25 22:14:42 +0530

    minute adjustments to the template

--- talentloop/src/components/resume-templates/ResumeTemplate.jsx
+++ talentloop/src/components/resume-templates/ResumeTemplate.jsx
@@ -65,20 +65,20 @@ const ResumeTemplate = ({ userData }) => {
 
   return (
     <div className="max-w-2xl mx-auto py-8 px-10 bg-white shadow-lg border border-gray-300 rounded-lg relative flex flex-col overflow-hidden">
-      {/* Background Image */}
       <div className="absolute inset-0 z-0 flex justify-center">
         <Image 
           src="/resume-template/Modern-shapes-of-abstract-banner-on-transparent-background-PNG.png" 
           alt="" 
           layout="fill" 
           objectFit="cover" 
-          className="opacity-15"
+          className="opacity-15 select-none"
+          draggable="false"
         />
       </div>
 
       {/* Profile and Contact Info */}
       <div className="flex w-full justify-between items-center z-10 relative mt-12">
-        <Image src={profile_picture} width={76} height={76} alt="" className="rounded-full border border-gray-300 shadow-md" />
+        <Image src={profile_picture} width={76} height={76} alt="" draggable='false' className="select-none rounded-full border border-gray-300 shadow-md" />
         
         <div className="flex flex-col items-start gap-[6px] text-right">
           <p className="text-xs font-medium text-orange-600">{location}</p>
@@ -90,7 +90,7 @@ const ResumeTemplate = ({ userData }) => {
       {/* About Section */}
       <div className="flex w-full mt-12 relative z-10">
         <div>
-          <Image src='/resume-template/side-ways-about.png' width={25} height={570} alt="ABOUT"/>
+          <Image src='/resume-template/side-ways-about.png' width={25} height={570} alt="ABOUT" draggable='false' className="select-none"/>
         </div>
 
         <div className="flex flex-col w-full items-start pl-10">
@@ -103,10 +103,8 @@ const ResumeTemplate = ({ userData }) => {
       {/* Experience Section */}
       <div className="flex w-full mt-12 relative z-10">
         <div>
-          <Image src='/resume-template/side-ways-work-experience.png' width={25} height={570} alt="EXPERIENCE"/>
+          <Image src='/resume-template/side-ways-work-experience.png' width={25} height={570} alt="EXPERIENCE" draggable='false' className="select-none"/>
         </div>
-
-        {/* Experience List - Add pl-10 like in About section */}
         <div className="flex flex-col w-full pl-10 items-start gap-6">
           {experience.map((exp, index) => (
             <div key={index} className="flex flex-col w-full">
@@ -130,7 +128,7 @@ const ResumeTemplate = ({ userData }) => {
         {/* Education Section */}
         <div className="flex w-5/12">
           <div>
-            <Image src='/resume-template/side-ways-education.png' width={20} height={570} alt="EDUCATION"/>
+            <Image src='/resume-template/side-ways-education.png' width={20} height={570} alt="EDUCATION" draggable='false' className="select-none"/>
           </div>
           <div>
             <div className="flex flex-col w-full pl-10 items-start gap-6">
@@ -156,7 +154,7 @@ const ResumeTemplate = ({ userData }) => {
           {/* Skills Section */}
           <div className="flex w-6/12">
             <div>
-              <Image src='/resume-template/side-ways-skills.png' width={20} height={570} alt="SKILLS"/>
+              <Image src='/resume-template/side-ways-skills.png' width={20} height={570} alt="SKILLS" draggable='false' className="select-none"/>
             </div>
             <div className="flex flex-col w-full pl-10 items-start gap-6">
               {skills.map((skill, index) => (
