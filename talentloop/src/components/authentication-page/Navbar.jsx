ef0afe0b7a7459a88f5f2aeab7b0e33f6db7ba20
aryannaik225 <aryann2203@gmail.com>
2025-02-26 00:44:11 +0530

    saving cause saw bunch of file changes

--- talentloop/src/components/authentication-page/Navbar.jsx
+++ talentloop/src/components/authentication-page/Navbar.jsx
@@ -1,7 +1,26 @@
+'use client'
+
+import { useAuth } from '@/context/AuthContext'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
 
 const Navbar = () => {
+
+  const { user, logOut } = useAuth()
+
+  const [showMenu, setShowMenu] = useState(false)
+  
+  const logOutFunc = async () => {
+    try {
+      await logOut()
+    } catch (error) {
+      console.log(error.message)
+    } finally {
+      setShowMenu(false)
+    }
+  }
+
   return (
     <div className='flex w-screen py-5 bg-white border-b border-black justify-between px-3 lg:px-5 xl:justify-center items-center gap-0 xl:gap-[142px]'>
       <a href="/">
@@ -15,10 +34,73 @@ const Navbar = () => {
         <span className='text-xs lg:text-sm text-[#1F1F1F] hover:text-[#4f4f4f] cursor-pointer transition-all duration-300'>Create Resume</span>
       </div>
 
-      <div className='flex gap-[22px] items-center poppins-semibold'>
-        <a href="/" className='text-xs lg:text-sm cursor-pointer hover:underline-offset-2 hover:underline transition-all duration-300 hover:scale-110'>Sign In</a>
-        <a href="/" className='rounded-md py-2 px-[18px] bg-[#0CA360] hover:bg-[#0CAF60] transition-all duration-300 hover:scale-110 text-xs lg:text-sm'>Sign Up</a>
-      </div>
+      <div className='hidden md:flex items-center gap-4'>
+          {!user ? (
+            <>
+              <button
+                onClick={() => windowChange('authentication-page')}
+                className='text-white poppins-semibold hover:text-[#a4a4a4] hover:scale-105 duration-300 ease-out cursor-pointer'
+              >
+                Sign In
+              </button>
+              
+              <button 
+                onClick={() => windowChange('authentication-page')}
+                className="hidden md:block poppins-semibold text-white py-2 px-4 rounded-md bg-[#0CA360] hover:bg-[#185037] transition-colors duration-300 ease-out"
+              >
+                Sign Up
+              </button>
+            </>
+          ) : (
+            <div className='flex items-center gap-4 relative'>
+              <button
+                onClick={() => setShowMenu(!showMenu)}
+                className='flex items-center gap-3 hover:scale-105 duration-300 ease-out cursor-pointer'
+              >
+                <motion.p 
+                  initial={{ scale: 2, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  transition={{ duration: 0.5, stiffness: 260, delay: 0.8 }}
+                  className='text-black text-sm poppins-semibold'
+                >
+                  {user.name}
+                </motion.p>
+                <motion.div
+                  initial={{ scale: 2, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  transition={{ duration: 0.5, stiffness: 260, delay: 1.2 }}
+                >
+                  <Image
+                    src={user.photoURL}
+                    width={40}
+                    height={40}
+                    alt="profile"
+                    className="rounded-full"
+                  />
+                </motion.div>
+              </button>
+
+              <div className={`absolute top-11 right-0 bg-[#33404c] w-40 ${showMenu ? 'flex' : 'hidden'} flex-col items-center py-4 gap-4 rounded-md`}>
+                <button
+                  onClick={() => windowChange('profile')}
+                  className='flex items-center gap-2 text-white text-sm poppins-semibold hover:text-[#a4a4a4] hover:scale-105 duration-300 ease-out cursor-pointer'
+                >
+                  <Image src='/landing-page/profile-icon.svg' width={24} height={24} alt='user' />
+                  Profile
+                </button>
+                <div className='w-10/12 h-[1px] bg-gray-500'/>
+                <button 
+                  onClick={logOutFunc}
+                  className='flex items-center gap-2 text-white text-sm poppins-semibold hover:text-[#a4a4a4] hover:scale-105 duration-300 ease-out cursor-pointer'
+                >
+                  <Image src='/landing-page/log-out.svg' width={24} height={24} alt='log-out' />
+                  Sign Out
+                </button>
+              </div>
+            </div>
+          )
+          }
+        </div>
 
     </div>
   )
